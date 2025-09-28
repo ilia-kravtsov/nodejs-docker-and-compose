36fe2c39874d95e26699568ab1ae6ff3be79f199
ilia-kravtsov <kravtsov.i.s@mail.ru>
2025-09-28 17:13:30 +0300

    feat: change environment variable names in config.ts

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -4,16 +4,16 @@ export default () => ({
   port: parseInt(process.env.PORT) || 3001,
   database: {
     type: process.env.DATABASE_TYPE || 'postgres',
-    host: process.env.DATABASE_HOST || '127.0.0.1',
-    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-    username: process.env.DATABASE_USER || 'student',
-    password: process.env.DATABASE_PASSWORD || 'student',
-    database: process.env.DATABASE_NAME || 'kupipodariday',
+    host: process.env.POSTGRES_HOST || 'postgres',
+    port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
+    username: process.env.POSTGRES_USER || 'student',
+    password: process.env.POSTGRES_PASSWORD || 'student',
+    database: process.env.POSTGRES_DB || 'kupipodariday',
     entities: [join(__dirname, '/../**/*.entity.{js,ts}')],
-    synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE) || true,
+    synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
   },
   jwt: {
-    key: process.env.JWT_KEY || 'defaultKey',
+    key: process.env.JWT_SECRET || 'defaultKey',
     expiresIn: process.env.JWT_EXPIRES_IN || '7d',
   },
 });
