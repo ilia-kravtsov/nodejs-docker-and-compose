153e24350939176fe7bc14badd822c0fa0278c23
ilia-kravtsov <kravtsov.i.s@mail.ru>
2025-09-30 18:47:21 +0300

    add: nginx default config, env variables to ecosystem

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   apps: [
     {
@@ -7,6 +9,20 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: '512M',
+      env: {
+        NODE_ENV: 'production',
+        PORT: process.env.PORT || 3001,
+        POSTGRES_HOST: process.env.POSTGRES_HOST,
+        POSTGRES_PORT: process.env.POSTGRES_PORT,
+        POSTGRES_USER: process.env.POSTGRES_USER,
+        POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+        POSTGRES_DB: process.env.POSTGRES_DB,
+        JWT_SECRET: process.env.JWT_SECRET,
+        JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+        DATABASE_TYPE: process.env.DATABASE_TYPE,
+        DATABASE_SYNCHRONIZE: process.env.DATABASE_SYNCHRONIZE,
+        POSTGRES_PGDATA: process.env.POSTGRES_PGDATA,
+      },
     },
   ],
 };
